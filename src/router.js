5cafa9a976fb699ba96cf2aed0303d73bc8688b8
WilliamHaxhija <williamhaxhija@gmail.com>
2024-07-02 15:27:18 +0200

    feat: aggiunto router

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import AppHome from './pages/AppHome.vue';
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome
+        }
+    ]
+});
+export { router };
