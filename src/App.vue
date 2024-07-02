0ae9fd1f95ed2e6a9a66b62f94df5ded209d7b55
WilliamHaxhija <williamhaxhija@gmail.com>
2024-07-02 15:21:19 +0200

    feat: aggiunto compnente AppHome

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
-
+import AppHome from "./pages/AppHome.vue";
+export default {
+  components: {
+    AppHome
+  }
+}
 </script>
 
 <template>
-
+<AppHome></AppHome>
 </template>
 
 <style lang="scss">
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,13 @@
+<script>
+
+export default {
+    name: 'AppHome'
+}
+
+</script>
+
+<template>
+<h1>CIAO</h1>
+</template>
+
+<style scoped lang="scss"></style>
