58ec56d7c5294d30e7de30cfe8ae875f5165a034
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-07-03 18:24:17 +0200

    feat: add header and hero

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,70 +1,70 @@
 <script>
 export default {
-    name:'AppHeader'
+    name: 'AppHeader'
 }
 </script>
 
 <template>
-    <section>
+    <section class="ms_hero">
         <header>
-           <div>
-             <!-- Navbar -->
-             <nav class="navbar navbar-expand-lg navbar-light shadow-sm p-1 mb-5 bg-body-tertiary rounded  ">
-            <!-- Container wrapper -->
-                <div class="container d-flex flex-wrap aligne-items-center justify-content-between">
-                    <!-- Toggle button -->
-                    <button
-                    data-mdb-collapse-init
-                    class="navbar-toggler"
-                    type="button"
-                    data-mdb-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                    >
-                    <i class="fas fa-bars"></i>
-                    </button>
-    
-                    <!-- Collapsible wrapper -->
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Our Logo -->
-                        <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                            <img
-                            src="../assets/img/logo_bnb.png"
-                            height="70"
-                            alt="MDB Logo"
-                            loading="lazy"
-                            />
-                        </a>
-                    
+            <div>
+                <!-- Navbar -->
+                <nav class="navbar navbar-expand-lg navbar-light shadow-sm p-1 mb-5 rounded  ">
+                    <!-- Container wrapper -->
+                    <div class="container d-flex flex-wrap aligne-items-center justify-content-between">
+                        <!-- Toggle button -->
+                        <button data-mdb-collapse-init class="navbar-toggler" type="button"
+                            data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars"></i>
+                        </button>
+
+                        <!-- Collapsible wrapper -->
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <!-- Our Logo -->
+                            <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                                <img src="../assets/img/logo_bnb.png" height="70" alt="MDB Logo" loading="lazy" />
+                            </a>
+
+                        </div>
+                        <!-- Collapsible wrapper -->
+
+                        <!-- Right elements -->
+                        <div class="d-flex align-items-center">
+
+                            <!-- Notifications -->
+
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Login</a></li>
+                                    <li><a class="dropdown-item" href="#">Registrati</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- Right elements -->
                     </div>
-                    <!-- Collapsible wrapper -->
-    
-                    <!-- Right elements -->
-                    <div class="d-flex align-items-center">
-    
-                        <!-- Notifications -->
-                       
-                    <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Login</a></li>
-                        <li><a class="dropdown-item" href="#">Registrati</a></li>
-                    </ul>
-                    </div>
-                    </div>
-                    <!-- Right elements -->
-                </div>
-            <!-- Container wrapper -->
-            </nav>
-            <!-- Navbar -->
-           </div>
+                    <!-- Container wrapper -->
+                </nav>
+                <!-- Navbar -->
+            </div>
         </header>
+        <main class="container">
+            
+        </main>
     </section>
 
 </template>
 
 <style scoped lang="scss">
- 
+.ms_hero {
+    min-height: 700px;
+    background-image: url(../assets/img/image_hero.webp);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
 </style>
