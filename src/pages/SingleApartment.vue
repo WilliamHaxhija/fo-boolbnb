a421584642bc3d4066c8de227d4eb3657323bceb
WilliamHaxhija <williamhaxhija@gmail.com>
2024-07-05 20:23:50 +0200

    feat: add ip address to db

--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -12,26 +12,44 @@ export default {
     },
     data() {
         return {
-            store
+            store,
+            ipAddress: null
         }
     },
     methods: {
-        getSingleApartment() {
-            axios.get(`${store.apiBaseUrl}/api/apartments/${this.$route.params.slug}`)
-                .then((response) => {
-                    store.apartment = response.data.apartment;
-                });
+        async getSingleApartment() {
+            try {
+                const response = await axios.get(`${store.apiBaseUrl}/api/apartments/${this.$route.params.slug}`);
+                store.apartment = response.data.apartment;
+            } catch (error) {
+                console.error('Errore durante il recupero dell\'appartamento:', error);
+            }
         },
-        postIpToApi() {
-            axios.post(`${store.apiBaseUrl}/visits`)
-                .then((response) => {
-                    this.ip = response.data.ip;
-                    this.apartmentId = store.apartment.id;
-                });
+        async getIpAddressFromApi() {
+            try {
+                const response = await axios.get('https://api.ipify.org/?format=json');
+                this.ipAddress = response.data.ip;
+            } catch (error) {
+                console.error('Si è verificato un errore:', error);
+            }
+        },
+        async sendIpAddressToBackend() {
+            try {
+                if (this.ipAddress && store.apartment && store.apartment.id) {
+                    const response = await axios.post(`${store.apiBaseUrl}/api/visits`, {
+                        ip_address: this.ipAddress,
+                        apartment_id: store.apartment.id
+                    });
+                }
+            } catch (error) {
+                console.error('Errore:', error);
+            }
         }
     },
-    mounted() {
-        this.getSingleApartment()
+    async mounted() {
+        await this.getSingleApartment();
+        await this.getIpAddressFromApi();
+        this.sendIpAddressToBackend();
     }
 }
 </script>
