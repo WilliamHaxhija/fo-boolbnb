c578774ec1049c98611b63c2f8921784f752a0ff
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-07-05 11:23:04 +0200

    feat: add style offcanvas

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -37,7 +37,7 @@ export default {
         Filtra appartamenti
     </button>
 
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+    <div class="offcanvas offcanvas-start rounded-4 my-3" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="staticBackdropLabel">Ecco i nostri servizi</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
