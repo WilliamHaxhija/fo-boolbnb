200cc5cc64a5e79cee22476f757e82873b705992
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-07-06 13:33:55 +0200

    feat: add button for sponsor

--- src/components/AppSponsorships.vue
+++ src/components/AppSponsorships.vue
@@ -9,9 +9,25 @@ export default {
     <section class="container mt-5">
         <h3 class="mb-5">Sponsorizzati</h3>
         <div class="d-flex gap-3 flex-wrap">
+            <!-- card -->
+            <div class="mb-5">
+                <div class="card">
+                    <div class="img-container">
+                        <img src="../assets/img/images.jpeg" alt="">
+                    </div>
+                    <div class="textBox">
+                        <p class="text head">Ethereum</p>
+                        <span>Cryptocurrency</span>
+                        <p class="text price">1.654,34€</p>
+                    </div>
+                </div>
+                <button class="button"></button>
+            </div>
+
+            <!-- card -->
             <div class="card mb-5">
                 <div class="img-container">
-                    <!-- <img src="../assets/img/images.jpeg" alt=""> -->
+                    <img src="../assets/img/images.jpeg" alt="">
                 </div>
                 <div class="textBox">
                     <p class="text head">Ethereum</p>
@@ -19,17 +35,7 @@ export default {
                     <p class="text price">1.654,34€</p>
                 </div>
             </div>
-            <div class="card mb-5">
-                <div class="img-container">
-                    <!-- <img src="../assets/img/images.jpeg" alt=""> -->
-                </div>
-                <div class="textBox">
-                    <p class="text head">Ethereum</p>
-                    <span>Cryptocurrency</span>
-                    <p class="text price">1.654,34€</p>
-                </div>
-            </div>
-            
+
 
         </div>
 
@@ -59,10 +65,6 @@ export default {
     align-items: center;
     justify-content: center;
     overflow: hidden;
-    background-image: url(../assets/img/images.jpeg);
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
 }
 
 .img-container img {
@@ -120,5 +122,69 @@ export default {
     100% {
         transform: translateY(0);
     }
+}
+
+// BUTTON
+.button {
+    position: relative;
+    background-color: transparent;
+    color: #e8e8e8;
+    font-size: 17px;
+    font-weight: 600;
+    border-radius: 10px;
+    width: 150px;
+    height: 30px;
+    border: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    overflow: hidden;
+    box-shadow: 0 10px 20px rgba(51, 51, 51, 0.2);
+    transition: all 0.3s cubic-bezier(0.23, 1, 0.320, 1);
+}
+
+.button::before {
+    content: "Hello!";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    background: linear-gradient(135deg, #7b4397, #dc2430);
+    transform: translate(0%, 90%);
+    z-index: 99;
+    position: relative;
+    transform-origin: bottom;
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1);
+}
+
+.button::after {
+    content: "Hover me";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #333;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    transform-origin: top;
+    transform: translate(0%, -100%);
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.320, 1);
+}
+
+.button:hover::before {
+    transform: translate(0%, 0%);
+}
+
+.button:hover::after {
+    transform: translate(0%, -200%);
+}
+
+.button:focus {
+    outline: none;
+}
+
+.button:active {
+    scale: 0.95;
 }
 </style>
