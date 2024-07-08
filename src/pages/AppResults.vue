d0c79147a83d0120bb04acefc3f3bf7ab5a7dff4
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-07-08 15:18:17 +0200

    fix

--- src/pages/AppResults.vue
+++ src/pages/AppResults.vue
@@ -22,12 +22,12 @@ export default {
 
     <section>
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 justify-content-center">
-            <div class="col d-flex align-items-stretch" v-for="apartment in store.searchedApartments"
-                :key="apartment.id">
-                <ApartmentCard :apartmentInfo="apartment"></ApartmentCard>
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                <div class="col d-flex align-items-stretch" v-for="apartment in store.searchedApartments"
+                    :key="apartment.id">
+                    <ApartmentCard :apartmentInfo="apartment"></ApartmentCard>
+                </div>
             </div>
-        </div>
         </div>
     </section>
 </template>
